0dc86713ff2764d54aefd1affb2bc44cd84e52c5
Lluís Bauzá <lluisbauzatogores@gmail.com>
2026-08-27 21:19:38 +0200

    chore: init pokeapi consume application and add pokeapi to application.properties

---
+++ activity-10-pokeapi-client/src/main/java/com/lluisbauza/activity10pokeapiclient/Activity10PokeapiClientApplication.java
@@ -0,0 +1,13 @@
+package com.lluisbauza.activity10pokeapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity10PokeapiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activity10PokeapiClientApplication.class, args);
+    }
+
+}
---
+++ activity-10-pokeapi-client/src/test/java/com/lluisbauza/activity10pokeapiclient/Activity10PokeapiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lluisbauza.activity10pokeapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity10PokeapiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
