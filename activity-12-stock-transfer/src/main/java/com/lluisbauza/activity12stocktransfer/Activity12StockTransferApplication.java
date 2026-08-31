9ad872168cd723ad0da43d70c0e1565931976bf5
Lluís Bauzá <lluisbauzatogores@gmail.com>
2026-08-31 11:11:21 +0200

    feat: init stock transfer between warehouses api

---
+++ activity-12-stock-transfer/src/main/java/com/lluisbauza/activity12stocktransfer/Activity12StockTransferApplication.java
@@ -0,0 +1,13 @@
+package com.lluisbauza.activity12stocktransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity12StockTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activity12StockTransferApplication.class, args);
+    }
+
+}
---
+++ activity-12-stock-transfer/src/test/java/com/lluisbauza/activity12stocktransfer/Activity12StockTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lluisbauza.activity12stocktransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity12StockTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
