c87808de76db979ca2c29009b45f26065d17fe91
Lluís Bauzá <lluisbauzatogores@gmail.com>
2026-08-31 19:16:02 +0200

    feat: init cinema seat manager api

---
+++ activity-13-cinema-seat-manager/src/main/java/com/lluisbauza/activity13cinemaseatmanager/Activity13CinemaSeatManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lluisbauza.activity13cinemaseatmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity13CinemaSeatManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activity13CinemaSeatManagerApplication.class, args);
+    }
+
+}
---
+++ activity-13-cinema-seat-manager/src/test/java/com/lluisbauza/activity13cinemaseatmanager/Activity13CinemaSeatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lluisbauza.activity13cinemaseatmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity13CinemaSeatManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
