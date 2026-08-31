4301229e32dc8dbc1a2f1f4e0a070b687b297f01
Lluís Bauzá <lluisbauzatogores@gmail.com>
2026-08-31 19:45:53 +0200

    chore: add seat entity

---
+++ activity-13-cinema-seat-manager/src/main/java/com/lluisbauza/activity13cinemaseatmanager/model/Seat.java
@@ -0,0 +1,21 @@
+package com.lluisbauza.activity13cinemaseatmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    
+    private String code;
+    private BigDecimal price;
+    private boolean booked;
+
+}
